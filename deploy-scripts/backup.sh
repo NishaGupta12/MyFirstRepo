c2b15ad36133157e2379e26d43d069d17d8367bc
NishaGupta12 <31597705+NishaGupta12@users.noreply.github.com>
2023-10-26 18:39:12 +0530

    Update backup.sh

--- deploy-scripts/backup.sh
+++ deploy-scripts/backup.sh
@@ -6,6 +6,6 @@ NOW_DATE=$(date '+%Y-%m-%d-%H-%M')
 RESTORE_FROM_INSTANCE_ID=database-1
 
 echo "Creating manual snapshot of ${RESTORE_FROM_INSTANCE_ID}"
-SNAPSHOT_ID=$( aws rds create-db-snapshot --db-snapshot-identifier $RESTORE_FROM_INSTANCE_ID-manual-$NOW_DATE --db-instance-identifier $RESTORE_FROM_INSTANCE_ID --query 'DBSnapshot.[DBSnapshotIdentifier]' --output text )
+SNAPSHOT_ID=$( aws rds create-db-snapshot --db-snapshot-identifier $RESTORE_FROM_INSTANCE_ID-manual-$NOW_DATE --db-instance-identifier $RESTORE_FROM_INSTANCE_ID --query 'DBSnapshot.[DBSnapshotIdentifier]' --output text --region 'us-east-1d' )
 aws rds wait db-snapshot-completed --db-snapshot-identifier $SNAPSHOT_ID
 echo "Finished creating new snapshot ${SNAPSHOT_ID} from ${RESTORE_FROM_INSTANCE_ID}"
