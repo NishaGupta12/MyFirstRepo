bda9f738df2b06ff0dcdd30c037e8313acf1ae17
NishaGupta12 <31597705+NishaGupta12@users.noreply.github.com>
2023-10-30 18:47:11 +0530

    Update backup.sh

--- deploy-scripts/backup.sh
+++ deploy-scripts/backup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 # set up some variables
 
 NOW_DATE=$(date '+%Y-%m-%d-%H-%M')
-RESTORE_FROM_INSTANCE_ID='database-1'
+RESTORE_FROM_INSTANCE_ID='myhearstdev'
 
 echo "Creating manual snapshot of ${RESTORE_FROM_INSTANCE_ID}"
-SNAPSHOT_ID=$( aws rds create-db-snapshot --db-snapshot-identifier $RESTORE_FROM_INSTANCE_ID-manual-$NOW_DATE --db-instance-identifier $RESTORE_FROM_INSTANCE_ID --query 'DBSnapshot.[DBSnapshotIdentifier]' --output text --region 'us-east-1' )
-aws rds wait db-snapshot-completed --db-snapshot-identifier $SNAPSHOT_ID
-echo "Finished creating new snapshot ${SNAPSHOT_ID} from ${RESTORE_FROM_INSTANCE_ID}"
+SNAPSHOT_ID_CLUSTER=$( aws rds create-db-cluster-snapshot  --db-cluster-identifier  $RESTORE_FROM_INSTANCE_ID-SSmanual-$NOW_DATE --db-cluster-snapshot-identifier $RESTORE_FROM_INSTANCE_ID --region 'us-east-1')
+aws rds wait db-cluster-snapshot-completed --db-cluster-snapshot-identifier $SNAPSHOT_ID_CLUSTER
+echo "Finished creating new Dev Server Cluster Snapshot ${SNAPSHOT_ID_CLUSTER} from ${RESTORE_FROM_INSTANCE_ID}"
