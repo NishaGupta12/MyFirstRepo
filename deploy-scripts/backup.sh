f50dcef673d3c01864dbbd3987fc668b6d394da6
NishaGupta12 <31597705+NishaGupta12@users.noreply.github.com>
2023-10-31 16:34:20 +0530

    Update backup.sh

--- deploy-scripts/backup.sh
+++ deploy-scripts/backup.sh
@@ -3,8 +3,6 @@
 
 NOW_DATE=$(date '+%Y-%m-%d-%H-%M')
 echo "this is testing"
-qa_rds_Cluster=#{BuildVariables.qa_rds_Cluster}
-ec2hostname=#{BuildVariables.ec2hostname}
 
 echo $ec2hostname
 echo $qa_rds_Cluster
